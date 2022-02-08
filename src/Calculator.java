895555b9f7eed5cbc1082c62d096b11c09cce7d5
asjMoria <Aiden.Moriarty@quinnipiac.edu>
2022-02-07 20:40:54 -0500

    I did task 3

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator
 		int opand1 = scanner.nextInt();
 		System.out.println("2:");
 		int opand2 = scanner.nextInt();
-		System.out.println("Would you like to add (+) or subtract (-) these two numbers? (please enter either the '+' or '-' symbol)");
+		System.out.println("Would you like to add (+), subtract (-), multiply (*) or divide (/) these two numbers? (please enter either the corresponding symbol)");
 		
 		Scanner scan = new Scanner(System.in);
 		//This is completely intentional, code will not work if I use the same Scanner
@@ -23,9 +23,17 @@ public class Calculator
 		{
 			result = opand1 + opand2;
 		}
-		else
+		else if (opor.equals("-"))
 		{
 			result = opand1 - opand2;
+		}
+		else if(opor.equals("*"))
+		{
+			result = opand1 * opand2;
+		}
+		else
+		{
+			result = opand1 / opand2;
 		}
 		
 		System.out.println("Your answer is: " + result + "!");
