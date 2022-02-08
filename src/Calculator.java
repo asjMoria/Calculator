506c807d52208416b6ea23059ee0a924382e63a5
asjMoria <Aiden.Moriarty@quinnipiac.edu>
2022-02-07 19:49:01 -0500

    I did task 1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Calculator 
 {
 	public static void main(String[] args)
 	{
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("Please enter two whole numbers (ints).");
+		System.out.println("1:");
+		int opand1 = scanner.nextInt();
+		System.out.println("2:");
+		int opand2 = scanner.nextInt();
+		int result = opand1 + opand2;
+		
+		System.out.println("Your answer is: " + result + "!");
 	}
 }
